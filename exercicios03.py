9840f366d1d3edd94e9770f182aa6af0594e8cb3
ferrzx <00001129651939sp@al.educacao.sp.gov.br>
2025-05-06 08:50:18 -0300

    criei um sistema para indentificar se sou maior de idade

---
+++ exercicios03.py
@@ -0,0 +1,7 @@
+def verificar_idade(nome, idade):
+    if idade >= 18:
+        return f"{nome} e maior de idade."
+    else:
+        return f"{nome} e maior de idade."
+    
+print(verificar_idade("feety", 77))
