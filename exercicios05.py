97cbe071bb717fe519f4c56a1459f72c1ce2cdd9
ferrzx <00001129651939sp@al.educacao.sp.gov.br>
2025-05-06 12:08:16 -0300

    sistema de cadastro junto com o de notas

--- exercicios05.py
+++ exercicios05.py
@@ -1,11 +1,13 @@
-def cadastrar_aluno(nome, email, serie):
+def cadastrar_aluno(nome, email, serie, nota01, nota02, nota03):
     alunos = []
     aluno = {
         "nome": nome,
         "email": email,
         "serie": serie,
+        "nota": [nota01, nota02, nota03]
     }
     
     alunos.append(aluno)
     return alunos
-print(cadastrar_aluno("xaulin matador de porco"," xaulinmatador@gmail.com", "2 tb"))
+
+print(cadastrar_aluno ("xaulinmatadordeporco", "xaulinmatador@gmail.com", "2° TB", 8, 9, 5))
