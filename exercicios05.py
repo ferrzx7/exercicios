e70b1bd67076f44a79e923d69ec55804e4c4ba1b
ferrzx <00001129651939sp@al.educacao.sp.gov.br>
2025-05-07 11:21:00 -0300

    fiz uma importacao de sistemas

--- exercicios05.py
+++ exercicios05.py
@@ -1,3 +1,5 @@
+from exercicios04 import somar_notas
+
 def cadastrar_aluno(nome, email, serie, nota01, nota02, nota03):
     alunos = []
     aluno = {
@@ -8,6 +10,7 @@ def cadastrar_aluno(nome, email, serie, nota01, nota02, nota03):
     }
     
     alunos.append(aluno)
+    media = somar_notas(aluno ["nota"])
     return alunos
 
 print(cadastrar_aluno ("xaulinmatadordeporco", "xaulinmatador@gmail.com", "2° TB", 8, 9, 5))
