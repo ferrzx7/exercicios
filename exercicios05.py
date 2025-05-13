88d49f886d3c02d39b097c8279ff0b637ce847ee
ferrzx <00001129651939sp@al.educacao.sp.gov.br>
2025-05-13 09:09:37 -0300

    atulizacao do sistema de cadastro e notas add informacoes com o sistema de codigo mais elaborado

--- exercicios05.py
+++ exercicios05.py
@@ -1,7 +1,18 @@
 from exercicios04 import somar_notas
 
+alunos = []
+
+def obter_dados_aluno():
+    nome_aluno = input("Informe o Nome Completo do Aluno: ")
+    email_aluno = input("Informe o Email Completo do Aluno: ")
+    serie_aluno = input("Informe a Serie Completo do Aluno: ")
+    nota01 = int(input("digite a nota01 do aluno: "))
+    nota02 = int(input("digite a nota02 do aluno: "))
+    nota03 = int(input("digite a nota03 do aluno: "))
+
+    return cadastrar_aluno(nome_aluno, email_aluno, serie_aluno, nota01, nota02, nota03)
+
 def cadastrar_aluno(nome, email, serie, nota01=0, nota02=0, nota03=0):
-    alunos = []
 
     notas = [nota01, nota02, nota03]
 
@@ -16,4 +27,13 @@ def cadastrar_aluno(nome, email, serie, nota01=0, nota02=0, nota03=0):
     alunos.append(aluno)
     media = somar_notas(aluno["nota"])
     return alunos
-print(cadastrar_aluno ("gustavo", "gus@gmail.com", "2°TB",8, 9, 8))
+
+obter_dados_aluno()
+
+def mostrar_dados_alunos(dados_alunos):
+    for aluno in dados_alunos:
+            print(f"Nome do Aluno: {aluno["nome"]}")
+
+    return print(dados_alunos)
+
+mostrar_dados_alunos(alunos)
